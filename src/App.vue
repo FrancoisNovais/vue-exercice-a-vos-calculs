b18b067ba35f92e3deb76d361aadcc2ef23c89ff
francois6381 <francois.novais@gmail.com>
2024-08-07 14:52:12 +0200

    PartOne composant

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+import PartOne from './components/PartOne.vue'
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <h1>A vos calculs !</h1>
+    <section>
+      <PartOne />
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/PartOne.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+const number = ref([76, 876, 467, 2, 67, 3, 90, 7, 678])
+
+const average = computed(() => {
+  let total = 0
+  for (let i = 0; i < number.value.length; i++) {
+    total += number.value[i]
+  }
+  total = total / number.value.length
+  return total.toFixed(2)
+})
+// console.log('Computed value ==> ', average.value)
+
+const pushRandomNumber = () => {
+  const randomNum = Math.floor(Math.random() * 1000)
+  //   console.log(randomNum)
+  return number.value.push(randomNum)
+}
+</script>
+
+<template>
+  <p>Moyenne du tableau : {{ number }}</p>
+  <p>{{ average }}</p>
+  <button @click="pushRandomNumber">Ajouter un nombre aléatoire</button>
+</template>
+
+<style scoped></style>
